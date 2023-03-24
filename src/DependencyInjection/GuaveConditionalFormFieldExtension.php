304cd5b2c32e2bc735ec2762bcf7aab18723557b
Christopher Stadler <christopher.stadler@guavestudios.com>
2023-03-24 09:03:32 +0100

    feature: update for contao 4.13 and prepare for 5

---
+++ src/DependencyInjection/GuaveConditionalFormFieldExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Guave\ConditionalFormFieldBundle\DependencyInjection;
+
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class GuaveConditionalFormFieldExtension extends Extension
+{
+    /**
+     * @throws Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../../config')
+        );
+        $loader->load('services.yaml');
+    }
+}
